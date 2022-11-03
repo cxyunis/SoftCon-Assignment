e10b6efd33636d2e5a2347b57a67c62684dd8be6
Xin Ying Chng <96065210+cxyunis@users.noreply.github.com>
2022-11-03 13:57:20 +0100

    Update InputSource.java

--- BattleshipGame/InputSource.java
+++ BattleshipGame/InputSource.java
@@ -1,10 +1,6 @@
-import java.util.Arrays;
-import java.util.List;
-
 public interface InputSource {
-    //column header
-    public final static List<String> COLHEADER = Arrays.asList("A","B","C","D","E","F","G","H","I","J");
+    //public final static List<String> COLHEADER = Arrays.asList("A","B","C","D","E","F","G","H","I","J");
 
-    String[] getShipPlacement(Ship ship);
+    String[] getShipPlacement(Ship s, int i);
     String getAttackAt();
 }
